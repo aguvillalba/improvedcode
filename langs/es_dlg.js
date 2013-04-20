7f393a9a4885632cfd6d64bcd3fbb669e61aa1cf
Agustin Villalba <avillalbacasas@gmail.com>
2013-04-20 17:43:52 +0200

    Added spanish langs

---
+++ langs/es_dlg.js
@@ -0,0 +1,7 @@
+tinyMCE.addI18n('es.improvedcode_dlg',{
+code_indented:"Indentar código",
+code_showHighlighting: "Mostrar resaltado",
+code_showLineNumbers: "Mostrar número de líneas",
+code_title:"ImprovedCode Editor de código HTML",
+code_wordwrap:"Ajustar líneas"
+});
